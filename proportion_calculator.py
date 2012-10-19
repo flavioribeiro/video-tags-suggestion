21afb8d5be8735e1f81ac84c832cc441807bd0a5
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2012-10-19 16:57:52 -0300

    Add api functions and some workarounds just for POC :)

--- proportion_calculator.py
+++ proportion_calculator.py
@@ -1,6 +1,9 @@
+#encoding: utf-8
 import redis
+import os
+import operator
 
-REDIS_HOST = "127.0.0.1"
+REDIS_HOST = "videos3s3.wm.globoi.com"
 REDIS_PORT = 6379
 
 def redis_handler():
@@ -24,9 +27,35 @@ def get_proportion(tag, term):
     return float(tag_count) / total
 
 def api_get_tags(term):
+    term = "term:" + term.lower()
     tags = get_tags(term)
     result = {}
     for tag in tags:
         result[tag] = get_proportion(tag, term)
 
     return result
+
+def api_get_merged_tags(text):
+    terms = tokenize_text(text)
+    terms = terms.split()
+
+    print "Terms: " + str(terms)
+    result = {}
+
+    for term in terms:
+        tags = api_get_tags(term)
+
+        for tag in tags.keys():
+            if result.has_key(tag):
+                result[tag] += tags[tag]
+            else:
+                result[tag] = tags[tag]
+
+    sorted_list = sorted(result.iteritems(), key=operator.itemgetter(1))
+    sorted_list.reverse()
+    return sorted_list
+
+def tokenize_text(text):
+    s = os.popen('java -jar /tmp/video-dump-1.0.0-SNAPSHOT-standalone.jar "' + str(text) + '"').read()
+
+    return s.replace('\n', '')
--- test_proportion.py
+++ test_proportion.py
@@ -1,7 +1,7 @@
 import pytest
 import requests
 import json
-from proportion_calculator import redis_handler, get_tags, get_proportion, api_get_tags
+from proportion_calculator import *
 
 
 def insert(term, tags):
@@ -34,3 +34,21 @@ def test_api_should_return_json_with_proportions_of_term():
 
     assert result == {"santos": 0.5263157894736842,"futebol": 0.42105263157894735, \
                      "dancinha": 0.05263157894736842}
+
+def test_api_should_return_json_with_tags_merged():
+    term = "neymar"
+    insert(term=term, tags={"santos": 100, "futebol": 80, "dancinha": 10})
+
+    term = "gols"
+    insert(term=term, tags={"futebol": 90, "santos": 1, "fluminense": 4})
+
+    tokenized_text = "neymar gols"
+
+    result = api_get_merged_tags(tokenized_text)
+
+    assert result == {"futebol": 1.3684210526315788, "santos":  0.5368421052631579, \
+			            "dancinha": 0.05263157894736842, "fluminense": 0.042105263157894736}
+
+
+def test_tokenize_text():
+    assert tokenize_text("anderson silva vence") == "silv venc anderson"
